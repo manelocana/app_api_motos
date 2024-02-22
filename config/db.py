c6fadcbd9aca841ea00509335608ff462616f3fb
manel <manelocana.dev@gmail.com>
2024-02-22 23:01:24 +0100

    passw añadido

--- config/db.py
+++ config/db.py
@@ -3,9 +3,10 @@
 
 # importamos
 from sqlalchemy import create_engine, MetaData
+from passw import passw
 
 # creamos el motor para la conexion con la db usando createengine
-engine = create_engine("mysql+pymysql://root:croke@localhost:3306/app_api_motos")
+engine = create_engine(f"mysql+pymysql://root:{passw}@localhost:3306/app_api_motos")
 
 # hacemos la conexion con sqlalchemy, usando metodo .connect()
 conn = engine.connect()
