bc45c3929de4bf0016fee709b710948a6aceb9ff
manel <manelocana.dev@gmail.com>
2024-03-23 22:48:08 +0100

    conexion a bd con POO

---
+++ pruebas/pruebas.py
@@ -0,0 +1,79 @@
+
+# database con terminal
+
+
+
+import pymysql
+#from config.passw import passw
+
+
+
+class Database:
+    def __init__(self):
+        self.connection = pymysql.connect(
+            host='localhost', #ip
+            user='root',
+            password='croke',
+            db='app_api_motos',
+        )
+
+        self.cursor = self.connection.cursor()
+        # probamos la conexion, si no da error, esta ok (python pruebas/pruebas.py)
+        print('Conectado a la DB')
+
+
+    def select_moto(self, id):
+        sql = 'SELECT id, marca, modelo FROM motos1  WHERE id = {}'.format(id)
+
+        try:
+            self.cursor.execute(sql)
+            user = self.cursor.fetchone()
+
+            print('Id: ', user[0])
+            print('Marca: ', user[1])
+            print('Modelo: ', user[2])
+        except Exception as e:
+            raise
+
+    
+    def select_all_motos(self):
+        sql = 'SELECT * FROM motos1'
+
+        try:
+            self.cursor.execute(sql)
+            users = self.cursor.fetchall()
+
+            for user in users:
+                print('Id: ', user[0])
+                print('Marca: ', user[1])
+                print('Modelo: ', user[2])
+                print('________\n')
+        except Exception as e:
+            raise
+
+
+    def update_moto(self, id, modelo):
+        sql = "UPDATE motos1 SET modelo='{}' WHERE id = '{}'".format(modelo, id)
+
+        try:
+            self.cursor.execute(sql)
+            self.connection.commit()
+        except Exception as e:
+            raise
+
+
+    def close(self):
+        self.connection.close()
+
+
+
+database = Database()
+
+database.select_moto(5)
+database.update_moto(5,'modelo pepii')
+database.select_moto(5)
+
+database.close()
+
+
+
