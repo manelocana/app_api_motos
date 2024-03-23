47ee7cad1bfe74b9b0e63360a778ce977ab0f290
manel <manelocana.dev@gmail.com>
2024-03-23 22:46:38 +0100

    añadido try, except

--- routers/motos.py
+++ routers/motos.py
@@ -18,37 +18,46 @@ async def hola():
 # funciona ok
 @motos_router.get("/motos")
 async def get_motos():
-    # conectamos a bd, hacemos select * en bd, (fetchall==*)
-    cursor = conn.execute(motosbd.select()).fetchall()
-    # sacamos los nombres de las columnas
-    column_names = [column.name for column in motosbd.columns]
-    # convertimos las filas a dict, usando los nombres d las columnas
-    motos_list = [dict(zip(column_names, row)) for row in cursor]
-    return {'motos_list':motos_list}
+    try:
+        cursor = conn.execute(motosbd.select()).fetchall()
+        column_names = [column.name for column in motosbd.columns]
+        # convertimos las filas a dict, usando los nombres d las columnas
+        motos_list = [dict(zip(column_names, row)) for row in cursor]
+        return {'motos_list':motos_list}
+    except Exception as e:
+        raise
 
-# funciona ok
+# no me da la moto completa, solo la id
 @motos_router.get("/motos/{id}")
 async def get_motillo(id: str):
-    result = conn.execute(motosbd.select().where(motosbd.c.id == id)).fetchone()
-    if result is None:
-        raise HTTPException(status_code=404, detail='no existe')
+    try:
+        result = conn.execute(motosbd.select().where(motosbd.c.id == id)).fetchone()
+        return dict(zip(id, result))
+    except Exception as e:
+        if result is None:
+            raise HTTPException(status_code=404, detail='no existe')
                 # con zip unimos los dos elementos
-    return dict(zip(id, result))
+    
     
 # añadir, funciona OK
 @motos_router.post('/motos')
 async def create_moto(motosbd: Moto):
     # new_moto = {'marca':moto.marca, 'modelo': moto.modelo, 'cilindrada': moto.cilindrada, 'año': moto.año, 'peso':moto.peso}
     new_moto = Moto
-    result = conn.execute(motosbd.insert().values(new_moto))
-    conn.commit()
-    return result.lastrowid, new_moto
+    try:
+        result = conn.execute(motosbd.insert().values(new_moto))
+        conn.commit()
+        return result.lastrowid, new_moto
+    except Exception as e:
+        raise
 
 # borrar, funciona OK
 @motos_router.delete("/motos/{id}")
 async def delete_moto(id: str):
-    result = conn.execute(motosbd.delete().where(motosbd.c.id == id))
-    conn.commit()
-    if result is None:
-        raise HTTPException(status_code=404, detail='no existe')
-    return  'id delete', id
+    try:
+        result = conn.execute(motosbd.delete().where(motosbd.c.id == id))
+        conn.commit()
+        return  'id delete', id
+    except Exception as e:
+        if result is None:
+            raise HTTPException(status_code=404, detail='no existe')
