8c98aa4479651231d2eec2e68ca0e8136fe5c8af
manel <manelocana.dev@gmail.com>
2024-04-11 22:35:12 +0200

    añadido el primary key en .id

--- models/motos.py
+++ models/motos.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql.sqltypes import String
 
 
 # creamos la tabla 
-motosbd = Table('motos1', meta, Column('id', String, autoincrement=True), 
+motosbd = Table('motos1', meta, Column('id', String, autoincrement=True, primary_key=True), 
               Column('marca', String(255)),
               Column('modelo', String(255)), 
               Column('cilindrada', String(255)), 
