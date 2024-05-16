c357e65d0bd57006b8965b389daad597f4869c47
manel <manelocana.dev@gmail.com>
2024-05-16 22:24:19 +0200

    retoques

--- crud/crud.py
+++ crud/crud.py
@@ -1,6 +1,5 @@
 # aqui voy a separar las funciones de las peticiones, para hacerlo mas facil de leer
 
-
 # aqui un crud core, directamente a la base datos sin usar orm
 
 
@@ -13,7 +12,7 @@ from models.motos import motosbd
 # presentacion
 def fun_hola():
     try:
-        return {'Welcome to the collection of motorcycles':'/docs in path for see documentation'}
+        return {'Welcome to the collection of motorcycles':'type /docs in path for see documentation'}
     except Exception as e:
         raise HTTPException(status_code=402, detail=str(e))
 
@@ -48,7 +47,6 @@ def see_motos():
 def find_moto(id: str):
     try:
         cursor = conn.execute(motosbd.select().where(motosbd.c.id == id)).fetchone()
-        # obtenemos los nombres de las tablas
         column_names = [column.name for column in motosbd.columns]
         return dict(zip(column_names, cursor))
     except Exception as e:
@@ -60,7 +58,6 @@ def find_moto(id: str):
 def borrar_moto(id:str):
     try:
         result = conn.execute(motosbd.select().where(motosbd.c.id == id)).fetchone()
-        # obtenemos los nombres de las tablas
         column_names = [column.name for column in motosbd.columns]
         resultado = dict(zip(column_names, result))
         return {'delete' : resultado}
